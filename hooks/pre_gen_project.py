744e518eb7305d09ed966ce0e4b68a86d96ae4e9
stan <adm.standev@gmail.com>
2025-08-10 21:51:50 +1200

    Refactor path handling to use relative paths.
    Replaced hardcoded absolute paths with dynamically generated relative paths to improve portability and flexibility of the script. Updated context print format to use JSON for better readability in outputs.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,21 @@
+import json
 from collections import OrderedDict
 from pathlib import Path
 
 
 def main():
     """Exemple de hook pré-génération."""
+    # Définir le chemin racine (répertoire actuel du fichier)
+    project_root = Path(
+        __file__
+    ).parent  # Pas d'utilisation de resolve() pour garder les chemins relatifs
+
+    # Chemins relatifs
+    template_dir = project_root / "template"
+    output_dir = template_dir / "test_generated_template"
+    repo_dir = project_root
+
+    # Créer le contexte avec chemins relatifs
     context = OrderedDict(
         {
             "project_name": "Mon super projet",
@@ -17,27 +29,15 @@ def main():
             "use_pre_commit": "yes",
             "python_version": "3.12",
             "author": "Anonyme <anonyme@example.com>",
-            "_template": str(
-                Path(
-                    "C:/Users/pc/Dropbox/Stan/PycharmProjects/cookiecutter-python-template"
-                ).resolve()
-            ),
-            "_output_dir": str(
-                Path(
-                    "C:/Users/pc/Dropbox/Stan/PycharmProjects/cookiecutter-python-template/test_generated_template"
-                ).resolve()
-            ),
-            "_repo_dir": str(
-                Path(
-                    "C:/Users/pc/Dropbox/Stan/PycharmProjects/cookiecutter-python-template"
-                ).resolve()
-            ),
+            "_template": str(template_dir).replace(str(project_root), "<project_root>"),
+            "_output_dir": str(output_dir).replace(str(project_root), "<project_root>"),
+            "_repo_dir": str(repo_dir).replace(str(project_root), "<project_root>"),
             "_checkout": None,
         }
     )
 
     # Éventuelles opérations supplémentaires sur le contexte
-    print(f"Contexte validé : {context}")
+    print(f"Contexte validé : {json.dumps(context, indent=4)}")
 
 
 if __name__ == "__main__":
